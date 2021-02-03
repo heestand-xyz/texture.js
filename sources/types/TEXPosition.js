0999c8ce24b767a519698a7818c4fb04ceb81f84
Anton Heestand <anton.heestand@gmail.com>
2021-02-03 10:08:34 +0100

    Cleanup

--- sources/types/TEXPosition.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var TEXPosition = /** @class */ (function () {
-    function TEXPosition(x, y) {
-        this.x = x;
-        this.y = y;
-    }
-    return TEXPosition;
-}());
-module.exports = TEXPosition;
